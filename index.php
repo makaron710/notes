b4ec6babc7fb43c24125c0020e7c0c106e3fc847
Максим Шаров <sm710mh@gmail.com>
2022-04-25 22:47:19 +0300

    first connection

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
   print('Hellow, World!');
+
+  /* Вы должны включить отчёт об ошибках для mysqli, прежде чем пытаться установить соединение */
+  mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+
+  $mysqli = new mysqli('localhost', 'maxim182_admin', 'Ololop_710', 'maxim182_notes');
+
+  /* Установите желаемую кодировку после установления соединения */
+  $mysqli->set_charset('utf8mb4');
+
+  printf("Успешно... %s\n", $mysqli->host_info);
